221a9b9171ee5f2e43c1a34065e580b296c13cfe
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-25 16:39:52 +0530

    perform crud in database using third party api

--- api-demo-app/app/controllers/posts_controller.rb
+++ api-demo-app/app/controllers/posts_controller.rb
@@ -1,9 +1,49 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  require 'rest-client'
   def index
-    posts = Post.all 
-    render json:{data: posts}
+    response = RestClient.get 'https://jsonplaceholder.typicode.com/posts'
+    render json: response.body, status: response.code
   end
   def create
-    Post.create(title: , body: )
+    all_params ={
+      "title": "#{params[:title]}",
+      "body": "#{params[:body]}",
+      "userId": "#{params[:user_id]}"
+    }
+    response = RestClient.post 'https://jsonplaceholder.typicode.com/posts', all_params
+    post = JSON.parse(response.body)
+    Post.create(title: post["title"], body: post["body"], user_id: post["userId"])
+    render json: response.body, status: response.code
+
   end
+
+  def show
+    response = RestClient.get "https://jsonplaceholder.typicode.com/posts/#{params[:id]}"
+    render json: response.body, status: response.code
+  end
+
+  def update
+    all_params ={
+      "title": "#{params[:title]}",
+      "body": "#{params[:body]}",
+      "userId": "#{params[:user_id]}"
+
+    }
+    response = RestClient.patch "https://jsonplaceholder.typicode.com/posts/#{params[:id]}", all_params
+    post = JSON.parse(response.body)
+    @post = Post.find_by(id: params[:id])
+    @post.update(title: post["title"], body: post["body"], user_id: post["userId"]) if @post.present?
+    render json: response.body, status: response.code
+  end
+
+  def destroy
+    response = RestClient.delete "https://jsonplaceholder.typicode.com/posts/#{params[:id]}"
+    post = JSON.parse(response.body)
+    debugger
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    render json: response.body, status: response.code
+  end
+
 end
