8b04554f1fdb68f613753c83294f90904b4e8747
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-26 15:04:00 +0530

    change routes

--- rails-jwt/config/routes.rb
+++ rails-jwt/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
+
     resources :users, param: :_username
     post '/auth/login', to: 'authentication#login'
     get '/*a', to: 'application#not_found'
-  end
+    root "users#index"
 end
