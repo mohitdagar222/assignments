67587b94e8ff3f2bcbef8ca1613f06aaf7b62599
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-29 17:43:27 +0530

    store tokens into db, create session as user log in, destroy session on signout

--- google-authentication/app/controllers/sessions_controller.rb
+++ google-authentication/app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
   def GoogleAuth
+      access_token = request.env["omniauth.auth"]
+      user = User.from_omniauth(access_token)
+      user.google_token = access_token.credentials.token
+      refresh_token = access_token.credentials.refresh_token
+      user.google_refresh_token = refresh_token if refresh_token.present?
+      user.save
+      session[:user_id] = user.id
     redirect_to "/"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
   end
 end
