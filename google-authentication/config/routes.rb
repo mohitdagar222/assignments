505f97e3fa3956799e32b53e7a90ffcd3e482ab9
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-29 17:44:18 +0530

    add path to sign out user

--- google-authentication/config/routes.rb
+++ google-authentication/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/GoogleAuth'
   get 'home/index'
+  get '/user/logout' => 'sessions#destroy'
   get 'auth/google_oauth2/callback', to: 'sessions#GoogleAuth'
   get 'auth/failure', to: redirect('/')
   root "home#index"
