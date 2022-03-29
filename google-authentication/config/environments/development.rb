b86525ec8169daf709b9a58d921d3d5e2125b6e0
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-29 16:06:36 +0530

    add client secrets

--- google-authentication/config/environments/development.rb
+++ google-authentication/config/environments/development.rb
@@ -7,6 +7,8 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  ENV['GOOGLE_CLIENT_ID'] = '907088366645-c639t9r480211br6tmqhp7r56u2cd9bt.apps.googleusercontent.com'
+  ENV['GOOGLE_CLIENT_SECRET'] = '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'
 
   # Do not eager load code on boot.
   config.eager_load = false
