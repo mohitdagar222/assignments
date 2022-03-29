3b2367495e8ea5cc0403aeab03acb0a5b8cf3ce2
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-29 16:03:03 +0530

    create home controller and index action to show button for login and make index action as root path

--- google-authentication/config/routes.rb
+++ google-authentication/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/GoogleAuth'
+  get 'home/index'
   get 'auth/google_oauth2/callback', to: 'sessions#GoogleAuth'
   get 'auth/failure', to: redirect('/')
+  root "home#index"
 end
