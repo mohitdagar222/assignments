88bc2bf958c973110b1ac3f260d4f4c92d074186
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-30 15:25:52 +0530

    create user on callback from github

--- github-authentication/app/controllers/sessions_controller.rb
+++ github-authentication/app/controllers/sessions_controller.rb
@@ -1,13 +1,9 @@
 class SessionsController < ApplicationController
   def GithubAuth
-    debugger
       access_token = request.env["omniauth.auth"]
-      user = User.from_omniauth(access_token)
-      user.google_token = access_token.credentials.token
-      refresh_token = access_token.credentials.refresh_token
-      user.google_refresh_token = refresh_token if refresh_token.present?
-      user.save
-      session[:user_id] = user.id
+      @user = User.from_omniauth(access_token)
+      @user.save
+      session[:user_id] = @user.id
     redirect_to "/"
   end
 
