2e50189c3d84e0ec1d9d38a782ce4d53b1e0db61
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-30 15:27:12 +0530

    add signout path in routes

--- github-authentication/config/routes.rb
+++ github-authentication/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'sessions/GithubAuth'
   root 'home#index'
   get 'auth/github/callback' => 'sessions#GithubAuth'
+  get '/user/logout' => 'sessions#destroy'
 
 end
