9e2b9b7fa6127cd28cb9c82166128562972c0b1b
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-31 18:29:56 +0530

    modify query to find user

--- twitter-authentication/app/controllers/application_controller.rb
+++ twitter-authentication/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     before_action :current_user
 	
     def current_user
-      @current_user = User.find(session[:user_id]) if session[:user_id].present?
+      @current_user = User.find_by(id: session[:user_id]) if session[:user_id].present?
     end
 end
