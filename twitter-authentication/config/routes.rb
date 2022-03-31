ea1e3a99a056eaaaa96e3fb2b5ec6b9f1c9b1339
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-31 17:15:22 +0530

    set path to sign out user

--- twitter-authentication/config/routes.rb
+++ twitter-authentication/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/TwitterAuth'
   root 'home#index'
+  get '/user/logout' => 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
  get 'auth/twitter/callback' => 'sessions#TwitterAuth'
   # Defines the root path route ("/")
