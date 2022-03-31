c3917ae57e7cfe60ae568ab3b850fadc5b9ce041
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-31 17:13:05 +0530

    create user model with method to create user and create users table

---
+++ twitter-authentication/app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+    def self.from_omniauth(auth)
+        # Creates a new user only if it doesn't exist
+        where(uid: auth['uid']).first_or_initialize do |user|
+            user.provider = auth['provider']
+            user.uid = auth['uid']
+            user.name = auth.info.nickname
+            user.profile_image = auth.info.image
+            user.token = auth.credentials.token
+            user.secret = auth.credentials.secret
+        end
+    end
+end
