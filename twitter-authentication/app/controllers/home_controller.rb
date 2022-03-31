a3b57c38fc90d11b5f337bb078a5a11f9a4f0106
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-31 17:16:29 +0530

    fetch user tweets

--- twitter-authentication/app/controllers/home_controller.rb
+++ twitter-authentication/app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+  require 'rest-client'
+
   def index
+    response =  RestClient.get 'https://api.twitter.com/2/users/1509451199979810820/tweets', {:Authorization => 'Bearer AAAAAAAAAAAAAAAAAAAAAA3bawEAAAAABfTGshMxeWxPIj7ydEwQ6vFpsQ8%3D64Qg3LkgcnjyzBQINpHfV46rfKc1cf7pQXFpf7ExXSqlOmw9uM'} 
+    @tweets = JSON.parse(response.body)
+    
   end
 end
