af1f74f6e4c12a10b0397457227bfd91b634b892
mohitdagar222 <mohitdagar222@gmail.com>
2022-03-31 17:16:57 +0530

    allow get request

--- twitter-authentication/config/initializers/omniauth.rb
+++ twitter-authentication/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :twitter, ENV['API_KEY'], ENV['API_SECRET']
+    provider :twitter, ENV['API_KEY'], ENV['API_SECRET'], scope: "openid profile email"
 end
 OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.silence_get_warning = true
