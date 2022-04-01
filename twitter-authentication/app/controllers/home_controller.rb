23b3c9813f8481841b4aa4db294e81d051c2ed60
mohitdagar222 <mohitdagar222@gmail.com>
2022-04-01 12:02:34 +0530

    make tweets dynamic according to current user

--- twitter-authentication/app/controllers/home_controller.rb
+++ twitter-authentication/app/controllers/home_controller.rb
@@ -2,8 +2,10 @@ class HomeController < ApplicationController
   require 'rest-client'
 
   def index
-    response =  RestClient.get 'https://api.twitter.com/2/users/1509451199979810820/tweets', {:Authorization => 'Bearer AAAAAAAAAAAAAAAAAAAAAA3bawEAAAAABfTGshMxeWxPIj7ydEwQ6vFpsQ8%3D64Qg3LkgcnjyzBQINpHfV46rfKc1cf7pQXFpf7ExXSqlOmw9uM'} 
-    @tweets = JSON.parse(response.body)
+    if @current_user.present?
+        response =  RestClient.get "https://api.twitter.com/2/users/#{@current_user.uid}/tweets", {:Authorization => 'Bearer AAAAAAAAAAAAAAAAAAAAAA3bawEAAAAABfTGshMxeWxPIj7ydEwQ6vFpsQ8%3D64Qg3LkgcnjyzBQINpHfV46rfKc1cf7pQXFpf7ExXSqlOmw9uM'} 
+        @tweets = JSON.parse(response.body)
+    end
     
   end
 end
